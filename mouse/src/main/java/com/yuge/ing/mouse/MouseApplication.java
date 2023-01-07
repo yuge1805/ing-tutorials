948b016fc8f33d7e97f6c356dea380a9b8f9650a
yuge1805 <yuge1805@yeah.net>
2023-01-07 09:38:50 +0800

    feat: mouse

---
+++ mouse/src/main/java/com/yuge/ing/mouse/MouseApplication.java
@@ -0,0 +1,13 @@
+package com.yuge.ing.mouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MouseApplication.class, args);
+    }
+
+}
---
+++ mouse/src/test/java/com/yuge/ing/mouse/MouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuge.ing.mouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
