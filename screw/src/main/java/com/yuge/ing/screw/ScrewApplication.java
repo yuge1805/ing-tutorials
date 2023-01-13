e23aef4323e37e5aa51b1b5cfce700418fc08d5f
yuge1805 <yuge1805@yeah.net>
2023-01-13 14:08:46 +0800

    feat: screw

---
+++ screw/src/main/java/com/yuge/ing/screw/ScrewApplication.java
@@ -0,0 +1,13 @@
+package com.yuge.ing.screw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrewApplication.class, args);
+    }
+
+}
