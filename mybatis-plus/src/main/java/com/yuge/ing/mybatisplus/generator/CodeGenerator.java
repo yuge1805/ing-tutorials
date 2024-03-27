030217f2ced5db327a7ed0055da662643565f10f
zhangbw <yuge1805@yeah.net>
2024-03-27 10:05:35 +0800

    feat: mybatis

--- mybatis-plus/src/main/java/com/yuge/ing/mybatisplus/generator/CodeGenerator.java
+++ mybatis-plus/src/main/java/com/yuge/ing/mybatisplus/generator/CodeGenerator.java
@@ -15,7 +15,7 @@ import java.io.File;
 public class CodeGenerator {
 
     public static void main(String[] args) {
-        cr();
+        shardingsphere();
     }
 
 
@@ -38,17 +38,50 @@ public class CodeGenerator {
             xmlOutputDir.concat(File.separator).concat(modulePackage);
         }
         DataSourceInfo info = new DataSourceInfo(
-                "jdbc:mysql://***:3306/***?useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&useSSL=false&serverTimezone=GMT%2B8",
-                "***",
-                "***");
+                "jdbc:mysql://192.168.1.64:3306/szxy_curriculum_arrangement?useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&useSSL=false&serverTimezone=GMT%2B8",
+                "szxy",
+                "Qtone*2023");
         GenerationParam generationParam = new GenerationParam()
-                .setRootPackage("cn.***")
+                .setRootPackage("cn.qtone.ecampus.curriculum.arrangement")
                 .setDataSourceInfo(info)
-                .setAuthor("***")
+                .setAuthor("zhangbw")
                 .setJavaOutputDir(javaOutputDir)
                 .setXmlOutputDir(xmlOutputDir)
                 .setPrefix("")
-                .setTableNames(Lists.newArrayList("***"));
+                .setTableNames(Lists.newArrayList("cas_tuning_record"));
+        CodeGenerationUtil.generation(generationParam);
+    }
+
+    private static void shardingsphere() {
+        String projectPath = System.getProperty("user.dir");
+        projectPath = "D:\\Workspace\\GitHub-Workspace\\ing-tutorials\\shardingsphere";
+        // 修改为对应module、package
+        String moduleName = "";
+        String modulePackage = "";
+        if (StringUtils.isNotBlank(moduleName)) {
+            projectPath = projectPath.concat(File.separator).concat(moduleName);
+        }
+        projectPath = projectPath.concat(File.separator).concat("src")
+                .concat(File.separator).concat("main")
+                .concat(File.separator);
+        String javaOutputDir = projectPath.concat("java");
+        String xmlOutputDir = projectPath.concat("resources")
+                .concat(File.separator).concat("mapper");
+        if (StringUtils.isNotBlank(modulePackage)) {
+            xmlOutputDir.concat(File.separator).concat(modulePackage);
+        }
+        DataSourceInfo info = new DataSourceInfo(
+                "jdbc:mysql://my.ing:13306/ds_0?useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&useSSL=false&serverTimezone=GMT%2B8",
+                "root",
+                "root");
+        GenerationParam generationParam = new GenerationParam()
+                .setRootPackage("com.yuge.ing.shardingsphere")
+                .setDataSourceInfo(info)
+                .setAuthor("yuge")
+                .setJavaOutputDir(javaOutputDir)
+                .setXmlOutputDir(xmlOutputDir)
+                .setPrefix("t_")
+                .setTableNames(Lists.newArrayList("t_order", "t_order_item", "t_user"));
         CodeGenerationUtil.generation(generationParam);
     }
 
